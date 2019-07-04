b79bffb13ab4dc9f4f47004005aeb4da05b446e4
artpolly1 <art.polly1@gmail.com>
2019-07-03 22:56:22 -0500

    updated link to articles

--- scripts/scraped.js
+++ scripts/scraped.js
@@ -1,33 +1,58 @@
 const request = require("request");
 const cheerio = require("cheerio");
 
-const scrape = function(cb) {
-    request( "https://www.espn.com/nba/", function(err, res, body) {
-        const $ =cheerio.load(body);
-        const articles = [];
+request("https://www.espn.com/nba/",(error,response,html) =>{
+    if(!error && response.statusCode == 200){
+        const $ = cheerio.load(html);
 
-        $(".article-header").each(function(i, element){
-            const title = $(this).children("h1").text.trim();
-            const link = $(this).children(".data-src").text().trim();
 
-            if(head && link){
-                let titleNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-                let linkNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+        const contentItem = $(".contentItem__titleWrapper");
+        // console.log(contentItem.html());
+        // console.log(contentItem.text());
+        // const output = contentItem.find("h1").text().trim();
+       
+        // 
+        const scrape = contentItem.find('h1').next().text();
+        console.log(scrape);
+    }
+});
 
-                let dataToAdd = {
+
+
+
+
+
+// const scrape = function(cb) {
+//     request("https://www.espn.com/nba/", (err, res, html) => {
+
+//     if(!error && Response.statusCode == 200){
+//         const $ =cheerio.load(html);
+//     }
+        
+//         const articles = [];
+
+//         $(".article-header").each(function(i, element){
+//             const title = $(this).children("h1").text.trim();
+//             const link = $(this).children(".data-src").text().trim();
+
+//             if(head && link){
+//                 let titleNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+//                 let linkNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+
+//                 let dataToAdd = {
                     
-                    titleline: titleNeat, 
-                    link: linkNeat
-                };
-                articles.push(dataToAdd);
+//                     titleline: titleNeat, 
+//                     link: linkNeat
+//                 };
+//                 articles.push(dataToAdd);
                 
-                }
-        });
-        cb(articles);
+//                 }
+//         });
+//         cb(articles);
 
-    });
-};
-console.log(scrape);
+//     });
+// };
+// console.log(scrape);
 
-module.exports = scrape;
+// module.exports = scrape;
 
